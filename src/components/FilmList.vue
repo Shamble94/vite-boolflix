2dbc70f73457a39a71dd8f9743b111e4fc68c599
Shamble94 <fratus.nicholas@gmail.com>
2024-01-15 12:25:15 +0100

    Milestone 4 completata

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -48,67 +48,79 @@ export default{
 
 </script>
 <template lang="">
-  <div>
-    <body>
+
       <div class="container">
 
         <div class="row container-films">
           
-          <!-- LISTA CONTENENTE I DATI DEI FILM -->
+          
           <h4 v-if="store.film.length <= 0"> FAI LA TUA PRIMA RICERCA </h4>
-          <div class="card col-3 " v-for="(films, index) in store.film" :key="index" > 
-              <div>
-              <img :src="getPoster(films)" alt="poster">
-            </div> 
-            <div class="container-info">
+          <div class="card col-3" v-for="(films, index) in store.film" :key="index" > 
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img :src="getPoster(films)" alt="poster">
+                </div>
 
-              <!-- TITOLO  -->
-             <h2>{{ films.title }} </h2>
-              <div>
-                <!-- TITOLO ORIGINALE  -->
-            {{ films.original_title }}
+                <div class="flip-card-back">
+                  <div class="container-info">
+
+                    <div class="back">
+                    
+                      <h5>{{ films.title }} </h5>
+                      <div>
+                      
+                        {{ films.original_title }}
+                      </div>
+                      <div>
+
+                      <span > <i :class="getFlag(films.original_language)"></i></span>
+                      </div>
+                      <div>
+                        <span> {{ films.overview }}</span>
+                      </div> 
+                      <div class="mb-2">
+                    
+                        <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 1 ? `giallo` : `bianco`"></i>
+                        <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 2 ? `giallo` : `bianco`"></i>
+                        <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 3 ? `giallo` : `bianco`"></i>
+                        <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 4 ? `giallo` : `bianco`"></i>
+                        <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 5 ? `giallo` : `bianco`"></i>
+                      </div>
+                    </div>
+                  </div>            
+                  
+                </div>
               </div>
-              <div>
-              <!-- LINGUA ORIGINALE --> 
-             
-              <span > <i :class="getFlag(films.original_language)"></i></span>
-              </div>
-              <div class="mb-2">
-              <!--  VOTO IN STELLE -->  
-                <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 1 ? `giallo` : ``"></i>
-                <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 2 ? `giallo` : ``"></i>
-                <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 3 ? `giallo` : ``"></i>
-                <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 4 ? `giallo` : ``"></i>
-                <i class="bi bi-star-fill" :class = "Reviews(films.vote_average) >= 5 ? `giallo` : ``"></i>
-              </div>
-            </div>            
+            </div>
           </div>
         </div>        
       </div>
-    </body>
 
-  </div>
 </template>
 <style lang ="scss" scoped>
 @use '../styles/generals.scss' as *;
 .row{
     padding:0;
   }
+.container{
+  position: relative
+}
 .container-films{
   
     display: flex;
     margin-top: 20px;
+   
     h4{
       margin: 30px 0px
     }
     img{
     width: 100%;
-    height: 400px
+    height: 400px;
+    
    
    }
-   .card{
-    margin: 0
-   }
+  
    .container-info{
     display: flex;
     justify-content: center;
@@ -122,6 +134,63 @@ export default{
    .giallo{
     color: yellow
    }
+   .bianco{
+    color: white
+   }
+   
+
+
+
+   .flip-card {
+  background-color: transparent;
+  width: 100%;
+  height: 400px;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+ 
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: black;
+  color: white;
+  transform: rotateY(180deg);
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+}
+ .card{
+  padding: 0;
+ }  
 }
 </style>
 
--- src/components/SeriesList.vue
+++ src/components/SeriesList.vue
@@ -44,80 +44,149 @@ export default{
 
 </script>
 <template lang="">
-  <div>
-    <body>
-      <!-- CONTAINER -->
-      <div class="container">
-        <!--  CONTENITORE DEI FILM -->
-        <div class="container-series">
-            <div class="row ">
 
-                
-                <h4 v-if="store.series.length >0"> SERIE TV</h4>
-                  <!-- LISTA CONTENENTE I DATI DEI FILM -->
-                  
-                    <div class="card col-3" v-for="(serie, index) in store.series" :key="index" > 
-                            <div>
-                                <img :src="getPoster(serie)" alt="poster">
-                            </div>      
-                        <div class="container-info">
+  <div class="container">
 
-                          <!-- TITOLO  -->
-                           {{ serie.name }} 
-                          <div>
-                          <!-- TITOLO ORIGINALE  -->
-                        {{ serie.original_name }}
-                          </div>
-                          <div>
-                          <!--div class="cardNGUA ORIGINALE --> 
-                     
-                          <span > <i :class="getFlag(serie.original_language)"></i></span>
-                          </div>
-                          <div class="mb-2">
-                            <!-- VOTO IN STELLE -->
-                            <i class="bi bi-star-fill" :class = "SeriesReviews(serie.vote_average) >= 1 ? `giallo` : ``"></i>
-                            <i class="bi bi-star-fill" :class = "SeriesReviews(serie.vote_average) >= 2 ? `giallo` : ``"></i>
-                            <i class="bi bi-star-fill" :class = "SeriesReviews(serie.vote_average) >= 3 ? `giallo` : ``"></i>
-                            <i class="bi bi-star-fill" :class = "SeriesReviews(serie.vote_average) >= 4 ? `giallo` : ``"></i>
-                            <i class="bi bi-star-fill" :class = "SeriesReviews(serie.vote_average) >= 5 ? `giallo` : ``"></i>
-                          </div>
-                    
-                        </div>        
-                </div>
+    <div class="row container-films">
+      
+      
+      <h4 v-if="store.series.length  > 0"> SERIE TV</h4>
+      <div class="card col-3" v-for="(series, index) in store.series" :key="index" > 
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img :src="getPoster(series)" alt="poster">
             </div>
+
+            <div class="flip-card-back">
+              <div class="container-info">
+
+                <div class="back">
+                
+                  <h5>{{ series.name }} </h5>
+                  <div>
+                  
+                    {{ series.original_name }}
+                  </div>
+                  <div>
+
+                  <span > <i :class="getFlag(series.original_language)"></i></span>
+                  </div>
+                  <div>
+                    <span> {{ series.overview }}</span>
+                  </div> 
+                  <div class="mb-2">
+                
+                    <i class="bi bi-star-fill" :class = "SeriesReviews(series.vote_average) >= 1 ? `giallo` : `bianco`"></i>
+                    <i class="bi bi-star-fill" :class = "SeriesReviews(series.vote_average) >= 2 ? `giallo` : `bianco`"></i>
+                    <i class="bi bi-star-fill" :class = "SeriesReviews(series.vote_average) >= 3 ? `giallo` : `bianco`"></i>
+                    <i class="bi bi-star-fill" :class = "SeriesReviews(series.vote_average) >= 4 ? `giallo` : `bianco`"></i>
+                    <i class="bi bi-star-fill" :class = "SeriesReviews(series.vote_average) >= 5 ? `giallo` : `bianco`"></i>
+                  </div>
+                </div>
+              </div>            
+              
+            </div>
+          </div>
         </div>
       </div>
-
-      
-    </body>
-
+    </div>        
   </div>
+
 </template>
 <style lang ="scss" scoped>
 @use '../styles/generals.scss' as *;
-.container-series{
-    display: flex;
-    margin-top: 20px;
-    h4{ 
-      margin: 30px 0px
-    }
-    
-    img{
-    width: 100%;
-    height: 400px
-   
-   }
-   .container-info{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    line-height: 30px;
-   }
-   .giallo{
-    color: yellow
-   }
+.row{
+padding:0;
+}
+.container{
+position: relative
+}
+.container-films{
+
+display: flex;
+margin-top: 20px;
+
+h4{
+  margin: 30px 0px
+}
+img{
+width: 100%;
+height: 400px;
+
+
 }
 
+.container-info{
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+line-height: 30px;
+}
+i{
+color: black
+}
+.giallo{
+color: yellow
+}
+.bianco{
+color: white
+}
+
+
+
+
+.flip-card {
+background-color: transparent;
+width: 100%;
+height: 400px;
+perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+position: relative;
+width: 100%;
+height: 100%;
+text-align: center;
+transition: transform 0.8s;
+transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+position: absolute;
+width: 100%;
+height: 100%;
+-webkit-backface-visibility: hidden; /* Safari */
+backface-visibility: hidden;
+
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+background-color: #bbb;
+color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+background-color: black;
+color: white;
+transform: rotateY(180deg);
+width: 100%;
+height: 100%;
+overflow-y: auto;
+}
+.card{
+padding: 0;
+}  
+}
 </style>
 
